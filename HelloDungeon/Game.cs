da47aa8e1f1547e6e1622bca6a6ca38e67a42dda
Braxton Prejean <s238008@students.aie.edu>
2023-09-15 12:01:30 -0500

    Practiced arrays. Added values together

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -38,6 +38,9 @@ namespace HelloDungeon
         Hero Cole;
         Hero PepperMint;
         Hero Kane;
+        Hero Player;
+
+
 
         void Start()
         {
@@ -81,7 +84,7 @@ namespace HelloDungeon
         }
 
         public void run()
-        { 
+        {
             void PrintStats(Hero hero)
             {
                 Console.WriteLine("Name: " + hero.Name);
@@ -91,35 +94,151 @@ namespace HelloDungeon
 
                 Console.Clear();
             }
-
-            Console.WriteLine("Pick your class player: ");
-
-            
         }
 
-        //End of the game
-        void End()
+        void Fight()
         {
-            Console.WriteLine("Thanks for playing");
+            bool IsDefending = true;
+            string battleChoice = GetInput("Choose an action", "Attack", "Defend", "run");
+
+            if (battleChoice == "1")
+            {
+                Kane.Health = Attack(Player, Kane);
+                Console.WriteLine("You grit your teeth.");
+            }
+            else if (battleChoice == "2")
+            {
+                Console.WriteLine("You get ready to defend");
+            }
+
+            if (IsDefending = true)
+            {
+                Player.Defense /= 5;
+            }
+
         }
 
-        internal void Run()
+        string GetInput(string prompt, string option1, string option2, string option3)
         {
-            throw new NotImplementedException();
+            string playerChoice = "";
+
+            Console.WriteLine(prompt);
+            Console.WriteLine("1: " + option1);
+            Console.WriteLine("2" + option2);
+            Console.WriteLine("3" + option3);
+            Console.Write(">");
+
+            playerChoice = Console.ReadLine();
+
+            return playerChoice;
+        }
+
+        void Update()
+        {
+            if (CurrentScene == 0)
+            {
+                Fight(ref Cole, ref PepperMint);
+
+                Console.Clear();
+
+                Fight(ref Cole, ref PepperMint);
+
+                Console.Clear();
+
+                Fight(ref Cole, ref PepperMint);
+
+                void HeroSelection()
+                {
+                    string HeroChoice = GetInput("Choose Your Hero", Cole.Name, PepperMint.Name, Kane.Name);
+
+                    if (HeroChoice == "1")
+                    {
+                        Player = Cole;
+                    }
+                    else if (HeroChoice == "2")
+                    {
+                        Player = PepperMint;
+                    }
+                    else if (HeroChoice == "3")
+                    {
+                        Player = Kane;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input");
+                        Console.WriteLine("Press any key to continue");
+                        Console.ReadKey(true);
+                        Console.Clear();
+                        return;
+                    }
+
+
+                }
+
+
+
+                //update - called every time the game loops 
+                //Used to update game logic like player input, character positions, game timers, etc.
+
+                //end - called after the main game loop exits.
+                //Used to clean up memory or display end game messages.
+
+                //start - called before the first loop
+                //Used tp intialize variables at the beginning of the game.
+            }
         }
 
 
+        void SetArrayValue(int[] arr, int index, int value)
+        {
+            int[] grades = new int[5] { 22, 54, 43, 78, 2 };
 
-        //update - called every time the game loops 
-        //Used to update game logic like player input, character positions, game timers, etc.
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine(grades[i]);
+            }
 
-        //end - called after the main game loop exits.
-        //Used to clean up memory or display end game messages.
+        }
 
-        //start - called before the first loop
-        //Used tp intialize variables at the beginning of the game.
+        void TotalValue(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+        } 
 
 
+        
 
+        void BattleScene1()
+        {
+
+            Start();
+
+            while (gameOver == false)
+            {
+                Update();
+            }
+        }
+
+        void BattleScene2()
+        {
+            Fight(ref Cole, ref PepperMint);
+
+            Console.Clear();
+
+            if (Cole.Health > 0 && PepperMint.Health <= 0)
+            {
+                Console.WriteLine("The winner is: " + Cole.Name);
+            }
+            else if (PepperMint.Health > 0 && Cole.Health <= 0)
+            {
+                Console.WriteLine("The winner is " + PepperMint.Name);
+            }
+        }
     }
 }
